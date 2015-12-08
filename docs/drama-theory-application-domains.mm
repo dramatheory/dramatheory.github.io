e3be1b94db87fcf694e3cdf15cb58c62a813a3d7
Saul Howard <saul@saulhoward.com>
2015-12-08 23:02:02 +0000

    mind map updates

---
+++ docs/drama-theory-application-domains.mm
@@ -0,0 +1,70 @@
+﻿<map version="0.9.0">
+    <node TEXT="DT Application Domains ">
+        <node TEXT="Psychology and
+personal
+relations">
+            <node TEXT="Dilemmas Galore forum ">
+                <node TEXT="DT1"></node>
+                <node TEXT="DT2"></node>
+            </node>
+            <node TEXT="The Dramaturg blog "></node>
+            <node TEXT="Decision Workshops "></node>
+        </node>
+        <node TEXT="Fiction ">
+            <node TEXT="Dilemmas Galore forum ">
+                <node TEXT="DT1"></node>
+                <node TEXT="DT2"></node>
+            </node>
+            <node TEXT="The Dramaturg blog"></node>
+        </node>
+        <node TEXT="Business &amp; Management ">
+            <node TEXT="Dilemmas Galore forum">
+                <node TEXT="DT 1 business forum "></node>
+                <node TEXT="DT2 business forum"></node>
+                <node TEXT="DT2 forum for
+organisations &amp;
+managers"></node>
+            </node>
+            <node TEXT="Publications ">
+                <node TEXT="A new Drama-Theoretic Method for Conflict
+Resolution in Management"></node>
+                <node TEXT="The Six Dilemmas of
+Collaboration"></node>
+                <node TEXT="Acting Strategically Using
+Drama Theory"></node>
+            </node>
+            <node TEXT="Websites with DT-based
+tools and services">
+                <node TEXT="Idea Sciences "></node>
+                <node TEXT="Decision Workshops"></node>
+                <node TEXT="Decision Mechanics"></node>
+            </node>
+        </node>
+        <node TEXT="Military">
+            <node TEXT="Dilemmas Galore forum ">
+                <node TEXT="DT1"></node>
+                <node TEXT="DT2"></node>
+            </node>
+            <node TEXT="Publications ">
+                <node TEXT="The Utility of Force"></node>
+            </node>
+            <node TEXT="The Dramaturg blog"></node>
+        </node>
+        <node TEXT="Political
+Science and
+management">
+            <node TEXT="The Dramaturg blog "></node>
+            <node TEXT="Courses">
+                <node TEXT="Graduate School of Political
+Management at George
+Washington University"></node>
+            </node>
+        </node>
+        <node TEXT="Drama
+Theory basics">
+            <node TEXT="Dilemmas Galore "></node>
+            <node TEXT="The Dramaturg blog"></node>
+            <node TEXT="Decision Workshops"></node>
+        </node>
+    </node>
+</map>
---
+++ docs/drama-theory-resources.mm
@@ -0,0 +1,46 @@
+﻿<map version="0.9.0">
+    <node TEXT="DT resources ">
+        <node TEXT="Websites with
+DT-based tools
+and services">
+            <node TEXT="Decision Workshops"></node>
+            <node TEXT="Decision Mechanics"></node>
+            <node TEXT="Idea Sciences "></node>
+        </node>
+        <node TEXT="Academic links">
+            <node TEXT="Graduate School of Political
+Management at George
+Washington University"></node>
+        </node>
+        <node TEXT="Software ">
+            <node TEXT="E2E"></node>
+            <node TEXT="Confrontation Manager "></node>
+            <node TEXT="Dilemma Explorer "></node>
+            <node TEXT="Confronteer"></node>
+        </node>
+        <node TEXT="Publications ">
+            <node TEXT="The Six Dilemmas of
+Collaboration"></node>
+            <node TEXT="The Utility of Force"></node>
+            <node TEXT="Acting Strategically Using
+Drama Theory"></node>
+            <node TEXT="Oedipus Decision Maker"></node>
+            <node TEXT="A new Drama-Theoretic Method for Conflict
+Resolution in Management"></node>
+            <node TEXT="Confrontation Analysis: How To Win
+Operations Other Than War"></node>
+        </node>
+        <node TEXT="Blogs &amp; Forums">
+            <node TEXT="Decision Mechanics blog"></node>
+            <node TEXT="The Dramaturgy blog"></node>
+            <node TEXT="Dilemmas Galore forum "></node>
+        </node>
+        <node TEXT="Application domains "></node>
+        <node TEXT="Introduction to
+Drama Theory">
+            <node TEXT="Drama Theory 1"></node>
+            <node TEXT="Drama Theory 2"></node>
+            <node TEXT="DT in Wikipedia "></node>
+        </node>
+    </node>
+</map>
